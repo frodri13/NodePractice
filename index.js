a01fac0621d96fa810bc6527b467565bec9c9373
Fabio <alessf13@gmail.com>
2021-09-24 15:22:10 +0200

    feat: events and http

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const Person = require("./person");
+// const Person = require("./person");
 
-const person1 = new Person("Fabio", 30);
+// const person1 = new Person("Fabio", 30);
 
-person1.greeting();
+// person1.greeting();
+
+const Logger = require("./logger");
+
+const logger = new Logger();
+
+logger.on("message", (data) => console.log("Called listener", data));
+
+logger.log("Hello World");
